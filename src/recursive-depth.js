3dfcec64224a877c6367e7027a29e85cbe57aba6
vvvvvvladislav <vvvvvvladislav@yandex.ru>
2022-05-03 02:13:30 +0300

    init: add solution of half of the tasks

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -13,10 +13,42 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    constructor(depth, kostyl){
+        this.depth = 0;
+		this.kostyl = []
+    }
+	getDepth()
+	{
+		return this.depth;
+	}
+	incDepth()
+	{
+		this.depth++;
+	}
+	setZero()
+	{
+		this.depth = 0;
+	}
+	calculateDepth(arr) {
+		let count = JSON.stringify(arr);
+		count = count.replace(/[^[\]]/g, "");
+		if (count != "")
+		{
+			let i = 0;
+			while (count != "") {
+				count = count.replace(/\[\]/g, "");
+				this.incDepth();
+				i++;
+			}
+			this.kostyl.push(i)
+		    this.calculateDepth(count);
+			this.calculateDepth(count);
+		}
+		const result = this.getDepth();
+		return this.kostyl[this.kostyl.length-1];
+	}
+	//у меня проблемы с обнулением переменной, поэтому я переписал всё так ужасно, сорри (плюс ещё время в обрез).
+	//изначально рекурсия была более рекурсивна, но увы я не смог совладать с обнулением depth, поэтому.. вышло как вышло, да
 }
 
 module.exports = {
--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,51 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+	if (!(Array.isArray(arr))) {
+		throw new Error("'arr' parameter must be an instance of the Array!")
+	}
+	let output = [];
+	let ignore = false;
+	let ignore_prev = false
+	for (let i = 0; i < arr.length; i++) {
+			if (arr[i] == "--discard-next")
+			{
+				ignore = true;
+			}
+			else if (arr[i] == "--discard-prev")
+			{
+				if ((typeof arr[i-1] == 'undefined') || (ignore_prev == true)){
+					
+				} else
+				{output.pop();}
+				ignore_prev = false;
+			}
+			else if (arr[i] == "--double-next")
+			{
+				if (typeof arr[i+1] == 'undefined') {
+					
+				} else
+				{output.push(arr[i+1]);}
+			}
+			else if (arr[i] == "--double-prev")
+			{
+				if ((typeof arr[i-1] == 'undefined') || (ignore_prev == true)) {
+					
+				} else
+				{output.push(arr[i-1]);}
+				ignore_prev = false;
+			}
+			else if (ignore == true)
+			{
+				ignore = false;
+				ignore_prev = true;
+			}
+			else {
+				output.push(arr[i]);
+			}
+	}
+	return output;
 }
 
 module.exports = {
